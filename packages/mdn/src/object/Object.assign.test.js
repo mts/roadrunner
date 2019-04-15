48b3950053e277ac322a3adbbaecfeda4bf1e79b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 21:33:14 +0200

    Object.assign()

---
+++ packages/mdn/src/object/Object.assign.test.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+describe('Array', () => {
+  test('assign()', () => {
+    const target = { a: 1, b: 2 }
+    const source = { b: 4, c: 5 }
+
+    const returnedTarget = Object.assign(target, source)
+    expect(target).toEqual({ a: 1, b: 4, c: 5 })
+    expect(returnedTarget).toEqual({ a: 1, b: 4, c: 5 })
+  })
+})
