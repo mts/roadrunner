54fcc72cbc27728a8cc12a560b43d89187f3233e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 20:35:10 +0200

    Array.lastIndexOf()

---
+++ packages/mdn/src/array/Array.lastIndexOf.test.js
@@ -0,0 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+
+describe('Array', () => {
+  test('lastIndexOf()', () => {
+    var animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo']
+
+    expect(animals.lastIndexOf('Dodo')).toEqual(3)
+    expect(animals.lastIndexOf('Tiger')).toEqual(1)
+  })
+})
