273a27a37c80537b6935598e5ce19c34009c6f6a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 19:35:00 +0200

    Array.filter()

---
+++ packages/mdn/src/array/Array.filter.test.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+describe('Array', () => {
+  test('filter()', () => {
+    var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+    const result = words.filter(word => word.length > 6);
+
+    expect(result).toEqual(["exuberant", "destruction", "present"])
+  })
+})
