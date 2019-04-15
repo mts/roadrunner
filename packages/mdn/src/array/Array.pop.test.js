88c787882c71a46bd7f83ff81b9358bcf5e8a47b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 20:41:21 +0200

    Array.pop()

---
+++ packages/mdn/src/array/Array.pop.test.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+
+describe('Array', () => {
+  test('pop()', () => {
+    var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato']
+
+    expect(plants.pop()).toEqual('tomato')
+    expect(plants).toEqual(['broccoli', 'cauliflower', 'cabbage', 'kale'])
+    expect(plants.pop()).toEqual('kale')
+    expect(plants).toEqual(['broccoli', 'cauliflower', 'cabbage'])
+  })
+})
