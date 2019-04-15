fdabb1c73b28b491ddc8a05a37ca87628761cab7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 21:06:07 +0200

    Array.sort()

---
+++ packages/mdn/src/array/Array.sort.test.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+describe('Array', () => {
+  test('sort()', () => {
+    var months = ['March', 'Jan', 'Feb', 'Dec']
+    var array1 = [1, 30, 4, 21, 100000]
+
+    expect(months.sort()).toEqual(['Dec', 'Feb', 'Jan', 'March'])
+    expect(array1.sort()).toEqual([1, 100000, 21, 30, 4])
+  })
+})
