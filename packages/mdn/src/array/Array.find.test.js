780a780f7de73e38b1e6ceede0ae86559e89310f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 19:37:14 +0200

    Array.find()

---
+++ packages/mdn/src/array/Array.find.test.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+describe('Array', () => {
+  test('filter()', () => {
+    var array1 = [5, 12, 8, 130, 44]
+
+    var found = array1.find(function(element) {
+      return element > 10
+    })
+
+    expect(found).toEqual(12)
+  })
+})
