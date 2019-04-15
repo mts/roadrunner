2207760e12dfcb83dbfd8e1e6cbbecbb0a96558d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 19:39:18 +0200

    Array.findIndex()

---
+++ packages/mdn/src/array/Array.findIndex.test.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+
+describe('Array', () => {
+  test('filter()', () => {
+    var array1 = [5, 12, 8, 130, 44]
+
+    function isLargeNumber(element) {
+      return element > 13
+    }
+
+    expect(array1.findIndex(isLargeNumber)).toEqual(3)
+  })
+})
