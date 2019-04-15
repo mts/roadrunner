db41a22404c359dfe161b4d5c50f820646fdbe59
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 19:30:16 +0200

    Array.every()

---
+++ packages/mdn/src/array/Array.every.test.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+describe('Array', () => {
+  test('every()', () => {
+    function isBelowThreshold(currentValue) {
+      return currentValue < 40;
+    }
+
+    var array1 = [1, 30, 39, 29, 10, 13];
+
+    expect(array1.every(isBelowThreshold)).toEqual(true)
+  })
+})
