2c9bb7ad28fbaf9bc82c8ca26528793fc8c621f1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 20:42:59 +0200

    Array.push()

---
+++ packages/mdn/src/array/Array.push.test.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+
+describe('Array', () => {
+  test('push()', () => {
+    var animals = ['pigs', 'goats', 'sheep']
+
+    expect(animals.push('cows')).toEqual(4)
+    expect(animals).toEqual(['pigs', 'goats', 'sheep', 'cows'])
+    expect(animals.push('chickens')).toEqual(5)
+    expect(animals).toEqual(['pigs', 'goats', 'sheep', 'cows', 'chickens'])
+  })
+})
