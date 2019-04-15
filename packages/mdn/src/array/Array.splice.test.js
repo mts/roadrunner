2afe29c4a5019f6baf504e352ec73992944fb923
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 21:08:16 +0200

    Array.splice()

---
+++ packages/mdn/src/array/Array.splice.test.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+describe('Array', () => {
+  test('splice()', () => {
+    var months = ['Jan', 'March', 'April', 'June']
+
+    months.splice(1, 0, 'Feb')
+    expect(months).toEqual(['Jan', 'Feb', 'March', 'April', 'June'])
+    months.splice(4, 1, 'May')
+    expect(months).toEqual(['Jan', 'Feb', 'March', 'April', 'May'])
+  })
+})
