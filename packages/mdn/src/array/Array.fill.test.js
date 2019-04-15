bc84d824f48b28cf5670c530b7e1df9dfc265464
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-04-15 19:33:42 +0200

    Array.fill()

---
+++ packages/mdn/src/array/Array.fill.test.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+
+describe('Array', () => {
+  test('fill()', () => {
+    var array1 = [1, 2, 3, 4];
+
+    expect(array1.fill(0, 2, 4)).toEqual([1, 2, 0, 0])
+    expect(array1.fill(5, 1)).toEqual([1, 5, 5, 5])
+    expect(array1.fill(6)).toEqual([6, 6, 6, 6])
+  })
+})
