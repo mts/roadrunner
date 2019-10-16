48dfb6fe03d9768541cca23370266e66bb708dc8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 23:09:55 +0200

    change locale

--- packages/mdn/src/object/Object.toLocaleString.test.js
+++ packages/mdn/src/object/Object.toLocaleString.test.js
@@ -3,7 +3,7 @@
 describe('Object', () => {
   test('toLocaleString()', () => {
     const date1 = new Date(Date.UTC(2012, 11, 20, 3, 0, 0))
-    expect(date1.toLocaleString('ar-EG')).toEqual('12/20/2012, 4:00:00 AM')
+    expect(date1.toLocaleString('en-EN')).toEqual('12/20/2012, 4:00:00 AM')
 
     // const number1 = 123456.789
     // expect(number1.toLocaleString('de-DE')).toEqual(123,456.789)
