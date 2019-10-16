29b06a8482a1b45378c1fd9122495396d9beb6b3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-10-16 22:34:22 +0200

    lint

--- packages/rxjs/src/operator/combination/combineAll.test.js
+++ packages/rxjs/src/operator/combination/combineAll.test.js
@@ -6,7 +6,7 @@ import { interval } from 'rxjs'
 
 describe('combination', () => {
   test('combineall()', done => {
-    let results = []
+    const results = []
     const expected = [
       ['Result (0): 0', 'Result (1): 0'],
       ['Result (0): 1', 'Result (1): 0'],
@@ -19,9 +19,9 @@ describe('combination', () => {
       ['Result (0): 4', 'Result (1): 4'],
     ]
 
-    //emit every 1s, take 2
+    // emit every 1s, take 2
     const source = interval(1).pipe(take(2))
-    //map each emitted value from source to interval observable that takes 5 values
+    // map each emitted value from source to interval observable that takes 5 values
     const example = source.pipe(
       map(val =>
         interval(1).pipe(
